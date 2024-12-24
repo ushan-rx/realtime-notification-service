eed6aa407e4c08ed22f5c098e2c3ba7dc86b571d
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-24 22:59:02 +0530

    feat: add sample event (SystemAlertEvent)

---
+++ src/main/java/org/ushan/realtimenotificationservice/events/SystemAlertEvent.java
@@ -0,0 +1,38 @@
+package org.ushan.realtimenotificationservice.events;
+
+import lombok.*;
+
+
+@Data
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class SystemAlertEvent extends NotificationEvent{
+
+    private AlertLevel alertLevel;
+
+    public SystemAlertEvent(String message, String recipient, AlertLevel alertLevel) {
+        super("SYSTEM_ALERT", message, "SYSTEM", recipient); // Passing null as sender
+        this.alertLevel = alertLevel;
+    }
+
+    /**
+     * Enum representing the levels of alerts in the system.
+     */
+    public static enum AlertLevel{
+        DEBUG,       // Detailed information for debugging purposes
+        TRACE,       // Fine-grained details capturing the flow of the program
+        INFO,        // General informational messages
+        NOTICE,      // Significant events requiring attention
+        WARN,        // Potential issues to address
+        ERROR,       // Errors that impact functionality but not system operation
+        ALERT,       // High-priority issues needing immediate attention
+        CRITICAL,    // Severe issues risking system instability or failure
+        FATAL,       // Critical errors causing system failure
+        SUCCESS,     // Successful completion of an operation
+        RECOVERY,    // Recovery from an error or failure
+        PENDING,     // Ongoing process awaiting completion
+        UNKNOWN      // Undetermined alert level
+    }
+}
