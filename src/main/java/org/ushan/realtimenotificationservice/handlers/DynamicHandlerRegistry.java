aec9e1a4026dbb4ee1e2aad5927f9808997580d0
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-25 23:53:31 +0530

    fix: update DynamicHandlerRegistry

--- src/main/java/org/ushan/realtimenotificationservice/handlers/DynamicHandlerRegistry.java
+++ src/main/java/org/ushan/realtimenotificationservice/handlers/DynamicHandlerRegistry.java
@@ -1,10 +1,12 @@
 package org.ushan.realtimenotificationservice.handlers;
 
+import org.springframework.stereotype.Component;
 import org.ushan.realtimenotificationservice.events.NotificationEvent;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class DynamicHandlerRegistry {
     private final List<NotificationHandler> handlers = new ArrayList<>();
     public void registerHandler(NotificationHandler handler) {
