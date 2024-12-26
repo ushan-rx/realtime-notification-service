e6ed83b3e953947554d3f31b92b9632be248c98f
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-26 14:06:42 +0530

    fix : add annotation to friend request handler

--- src/main/java/org/ushan/realtimenotificationservice/handlers/custom/FriendRequestHandler.java
+++ src/main/java/org/ushan/realtimenotificationservice/handlers/custom/FriendRequestHandler.java
@@ -1,11 +1,14 @@
 package org.ushan.realtimenotificationservice.handlers.custom;
 
+import org.springframework.stereotype.Component;
+import org.ushan.realtimenotificationservice.annotations.EventHandler;
 import org.ushan.realtimenotificationservice.events.NotificationEvent;
 import org.ushan.realtimenotificationservice.events.custom.FriendRequestEvent;
 import org.ushan.realtimenotificationservice.handlers.NotificationHandler;
 
 import java.util.Date;
-
+@Component
+@EventHandler("FRIEND_REQUEST")
 public class FriendRequestHandler implements NotificationHandler {
     @Override
     public void handle(NotificationEvent event) {
