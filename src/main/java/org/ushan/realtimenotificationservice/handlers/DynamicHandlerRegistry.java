8ae30f414fe9e420ba8f33a69b2b9567d8ab7cd7
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-26 13:42:59 +0530

    feat : add event handler registry

--- src/main/java/org/ushan/realtimenotificationservice/handlers/DynamicHandlerRegistry.java
+++ src/main/java/org/ushan/realtimenotificationservice/handlers/DynamicHandlerRegistry.java
@@ -1,22 +1,42 @@
 package org.ushan.realtimenotificationservice.handlers;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
+import org.ushan.realtimenotificationservice.annotations.EventHandler;
 import org.ushan.realtimenotificationservice.events.NotificationEvent;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class DynamicHandlerRegistry {
-    private final List<NotificationHandler> handlers = new ArrayList<>();
-    public void registerHandler(NotificationHandler handler) {
-        handlers.add(handler);
+    private final Map<String, NotificationHandler> handlerMap = new HashMap<>();
+    private final List<NotificationHandler> handlers;
+    private final DefaultNotificationHandler defaultHandler;
+
+    public DynamicHandlerRegistry(List<NotificationHandler> handlers, DefaultNotificationHandler defaultHandler) {
+        this.handlers = handlers;
+        this.defaultHandler = defaultHandler;
+    }
+
+    @PostConstruct
+    private void initializeHandlers() {
+        handlers.forEach(handler -> {
+            EventHandler annotation = handler.getClass().getAnnotation(EventHandler.class);
+            if (annotation != null) {
+                handlerMap.put(annotation.value(), handler);
+            }
+        });
     }
 
     public void handleEvent(NotificationEvent event) {
-        handlers.stream()
-                .filter(handler -> handler.canHandle(event.getType()))
-                .findFirst()
-                .ifPresent(handler -> handler.handle(event));
+        NotificationHandler handler = handlerMap.get(event.getType());
+        if (handler != null) {
+            handler.handle(event);
+        } else {
+            System.out.println("No handler found for event type: " + event.getType());
+            defaultHandler.handle(event);
+        }
     }
 }
