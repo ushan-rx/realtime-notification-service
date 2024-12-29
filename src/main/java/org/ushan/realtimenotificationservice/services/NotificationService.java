8140642088db97c6ea0a27733b4756d1d468353d
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-29 21:49:58 +0530

    fix: resolve dependencies (logback)

--- src/main/java/org/ushan/realtimenotificationservice/services/NotificationService.java
+++ src/main/java/org/ushan/realtimenotificationservice/services/NotificationService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.ushan.realtimenotificationservice.events.BroadcastNotification;
-import org.ushan.realtimenotificationservice.events.NotificationEvent;
 import org.ushan.realtimenotificationservice.events.UserSpecificNotification;
 
 @Service
