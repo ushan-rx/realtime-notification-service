dd2051b8398e4bd236ed1174adb2ca1382e3bcf6
ushan senarathna <ushansenarathna000@gmail.com>
2024-12-29 21:38:29 +0530

    Revert "feat : add exception handling and logging to NotificationService"
    This reverts commit 0fc47818b9a05679c5038e28ba927de27e5abaad.

--- src/main/java/org/ushan/realtimenotificationservice/services/NotificationService.java
+++ src/main/java/org/ushan/realtimenotificationservice/services/NotificationService.java
@@ -1,17 +1,14 @@
 package org.ushan.realtimenotificationservice.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.ushan.realtimenotificationservice.events.BroadcastNotification;
+import org.ushan.realtimenotificationservice.events.NotificationEvent;
 import org.ushan.realtimenotificationservice.events.UserSpecificNotification;
 
 @Service
 public class NotificationService {
-
-    private static final Logger logger = LoggerFactory.getLogger(NotificationService.class);
     private final SimpMessagingTemplate messagingTemplate;
 
     @Autowired
@@ -20,24 +17,12 @@ public class NotificationService {
     }
 
     public <T extends UserSpecificNotification> void sendNotificationToUser(T event, String userId) {
-        try {
-            String userTopic = "/user/" + userId + "/queue";
-            logger.info("Sending user-specific notification to userId: {} with event: {}", userId, event);
-            event.handle(messagingTemplate, userTopic, userId);
-        } catch (Exception e) {
-            logger.error("Failed to send user-specific notification", e);
-            throw e;
-        }
+        String userTopic = "/user/" + userId + "/queue";
+        event.handle(messagingTemplate, userTopic, userId);
     }
 
     public  <T extends BroadcastNotification> void sendBroadcastNotification(T event) {
-        try {
-            String broadcastTopic = "/topic";
-            logger.info("Sending broadcast notification with event: {}", event);
-            event.handle(messagingTemplate, broadcastTopic);
-        } catch (Exception e) {
-            logger.error("Failed to send broadcast notification", e);
-            throw e;
-        }
+        String broadcastTopic = "/topic";
+        event.handle(messagingTemplate, broadcastTopic);
     }
 }
